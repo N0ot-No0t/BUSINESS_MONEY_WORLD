e376b47db72a75d32ad2534429d655cb1411b490
N0ot-No0t <sustaccosmin544@hotmail.ca>
2018-04-10 12:04:31 -0400

    added bank

---
+++ app/src/main/java/com/patateco/qc/ca/businessmoneyworld/Bank.java
@@ -0,0 +1,15 @@
+package com.patateco.qc.ca.businessmoneyworld;
+
+public class Bank {
+
+    int montantRetire = 0;
+    int montantDepose = 0;
+    int tauxInteret = 0;
+    public Bank(int montantDepose, int montantRetire){
+
+        this.montantDepose = montantDepose;
+        this.montantRetire = montantRetire;
+
+    }
+
+}
