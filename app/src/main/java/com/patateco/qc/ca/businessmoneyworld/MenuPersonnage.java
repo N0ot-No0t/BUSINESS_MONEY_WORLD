7667d30a27c2aa27f5194561a282f09295620594
adriann99 <37342361+adriann99@users.noreply.github.com>
2018-04-10 10:14:17 -0400

    pers

--- app/src/main/java/com/patateco/qc/ca/businessmoneyworld/MenuPersonnage.java
+++ app/src/main/java/com/patateco/qc/ca/businessmoneyworld/MenuPersonnage.java
@@ -3,6 +3,10 @@ package com.patateco.qc.ca.businessmoneyworld;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -16,10 +20,7 @@ public class MenuPersonnage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.pop_pers);
-
-
 
         DisplayMetrics dm = new DisplayMetrics();
 
@@ -31,9 +32,15 @@ public class MenuPersonnage extends Activity {
 
         getWindow().setLayout((int)(width*.8),(int)(height*.6));
 
-
-
-
+String name = null;
+if (name == null) {
+    TextView textView = (TextView) findViewById(R.id.enterName);
+    textView.setText("Enter your name: ");
+    EditText inputTxt = (EditText) findViewById(R.id.persName);
+    name = inputTxt.getText().toString();
+}
+        TextView textView = (TextView) findViewById(R.id.nameDisplay);
+        textView.setText(name);
 
 
     }
